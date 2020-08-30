9ece3b2ffc01f1720f2ec963f753a23ba5ad4baf
Ian Nater <ian.nater@gmail.com>
2020-08-29 21:18:47 -0500

    Added the icon for weather and did some styling to the page. Stil have a bug with the first day and need to add dates and UV color section

--- develop/java.js
+++ develop/java.js
@@ -2,8 +2,12 @@ $(document).ready(function () {
     $(".searchBtn").on("click", function () {
         var value = $("#input").val();
         $("#input").val("");
+        $(".currentWeather").empty();
+        
+
         searchWeather(value);
-        //searchFiveDay(value);
+        
+        
     });
 
     function rowMake(city) {
@@ -29,9 +33,9 @@ $(document).ready(function () {
 
             var cardContent = $("<div>");
             var name = $("<h3>").text(res.name);
-            var temp = $("<p>").text("Temperature: " + res.main.temp + " F");
-            var humidity = $("<p>").text("Humidity: " + res.main.humidity + "%");
-            var wind = $("<p>").text("Wind Speed: " + res.wind.speed + " MPH");
+            var temp = $("<h4>").text("Temperature: " + res.main.temp + " F");
+            var humidity = $("<h4>").text("Humidity: " + res.main.humidity + "%");
+            var wind = $("<h4>").text("Wind Speed: " + res.wind.speed + " MPH");
             var long = res.coord.lon
             var lat = res.coord.lat
 
@@ -51,34 +55,25 @@ $(document).ready(function () {
                 url: `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&exclude={part}&appid=${apiKey}&units=imperial`,
                 dataType: "json"
             }).then(function (res) {
-                console.log(res)
-                var uVindex = $("<p>").text("UV Index: " + res.current.uvi);
-                var day1 = $("<p>").text("Temp: " + res.daily[0].temp.day + " F");
-                var humid1= $("<p>").text("Humidity: " + res.daily[0].humidity + " %");
-                var day2 = $("<p>").text("Temp: " + res.daily[1].temp.day + " F");
-                var humid2= $("<p>").text("Humidity: " + res.daily[1].humidity + " %");
-                var day3 = $("<p>").text("Temp: " + res.daily[2].temp.day + " F");
-                var humid3= $("<p>").text("Humidity: " + res.daily[2].humidity + " %");
-                var day4 = $("<p>").text("Temp: " + res.daily[3].temp.day + " F");
-                var humid4= $("<p>").text("Humidity: " + res.daily[3].humidity + " %");
-                var day5 = $("<p>").text("Temp: " + res.daily[4].temp.day + " F");
-                var humid5= $("<p>").text("Humidity: " + res.daily[4].humidity + " %");
 
+                var uVindex = $("<h4>").text("UV Index: " + res.current.uvi);
 
-                $("#day1").append(day1);
-                $("#day1").append(humid1);
-                $("#day2").append(day2);
-                $("#day2").append(humid2);
-                $("#day3").append(day3);
-                $("#day3").append(humid3);
-                $("#day4").append(day4);
-                $("#day4").append(humid4);
-                $("#day5").append(day5);
-                $("#day5").append(humid5);
-                
+                for (let index = 0; index <= 4; index++)  {
+                    var dayNumber = index +1
+                    var fiveDayPic = res.daily[index].weather[0].icon
+                    var day = $("<p>").text("Temp: " + res.daily[index].temp.day + " F");
+                    var humidity = $("<p>").text("Humidity: " + res.daily[index].humidity + " %");
+                    var imageEl = $("<img>").attr("src", fiveDayIcon).attr("style", "width: 50px")
+                    var fiveDayIcon = "https://openweathermap.org/img/wn/" + fiveDayPic + ".png"
+                    
+                    $("#day" + dayNumber).append(day)
+                    $("#day" + dayNumber).append(humidity)
+                    $("#day" + dayNumber).append(imageEl)
+                    
+
+                }
+            
                 cardContent.append(uVindex)
-
-          
 
             })
 
